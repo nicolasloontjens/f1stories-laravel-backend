a954259798000d5efdeab0ec391e127c5a94709a
NicolasLoontjens <nicolas.loontjens@student.howest.be>
2022-04-05 15:31:43 +0200

    added get comments endpoint

--- app/Modules/Comments/Services/CommentService.php
+++ app/Modules/Comments/Services/CommentService.php
@@ -6,25 +6,30 @@ use App\Modules\Comments\Models\Comment;
 use App\Modules\Core\Services\Service;
 use App\Modules\Stories\Models\Story;
 use App\Models\User;
+use stdClass;
 
 class CommentService extends Service{
-
-    protected $rules = [
-        "username" => "required",
-        "password" => "required"
-    ];
 
     public function __construct(Comment $model){
         parent::__construct($model);
     }
 
     public function all($storyid){
-        return Story::find($storyid)->user()->get();
-
-        return $this->model->where('story_id',$storyid);
+        $comments = Comment::with('user')->where('story_id',$storyid)->get();
+        $res = [];
+        foreach($comments as $comment){
+            $res[] = $this->convertToRightFormat($comment);
+        }
+        return $res;
     }
 
     public function convertToRightFormat($comment){
-
+        $c = new stdClass;
+        $c->commentid = $comment['id'];
+        $c->userid = $comment['user']['id'];
+        $c->storyid = $comment['story_id'];
+        $c->content = $comment['content'];
+        $c->username = $comment['user']['username'];
+        return $c;
     }
 }
